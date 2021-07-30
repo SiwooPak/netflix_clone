bdb4ec089bb96cc5b5ff0a079313dfc415876c69
SiwooPak <bogus1620@naver.com>
2021-07-30 22:44:35 +0900

    사용할 api의 주소 설정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// moviedb api
+export const API_URL = 'https://api.themoviedb.org/3/';
+// moviedb image
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
